28450e51ab35b9d66ad09c2851d08af7ac244d07
AndoniMendoza <2707802540719@ingenieria.usac.edu.gt>
2023-09-29 00:07:08 -0600

    otra modificacion en codigo

--- codigo.py
+++ codigo.py
@@ -21,6 +21,7 @@ def division(a, b):
 def calculadora():
     print("Selecciona una operación:")
     #print("1. Suma")
+    print("otras opciones")
     print("2. Resta")
     print("3. Multiplicación")
     print("4. División")
